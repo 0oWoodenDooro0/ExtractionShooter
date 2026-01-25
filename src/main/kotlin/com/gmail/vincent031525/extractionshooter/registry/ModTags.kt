18e595972d07e521e360d8cf9d720a8dca620f95
Vincent <vincent031525@gmail.com>
2026-01-25 15:02:25 +0800

    feat(registry): Define new equipment and gun TagKeys

--- src/main/kotlin/com/gmail/vincent031525/extractionshooter/registry/ModTags.kt
+++ src/main/kotlin/com/gmail/vincent031525/extractionshooter/registry/ModTags.kt
@@ -8,4 +8,9 @@ import net.minecraft.world.item.Item
 object ModTags {
     val HELMETS: TagKey<Item> = TagKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath("extractionshooter", "helmets"))
     val ARMORS: TagKey<Item> = TagKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath("extractionshooter", "armors"))
+    val BACKPACKS: TagKey<Item> = TagKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath("extractionshooter", "backpacks"))
+    val RIGS: TagKey<Item> = TagKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath("extractionshooter", "rigs"))
+    val PRIMARY_WEAPONS: TagKey<Item> = TagKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath("extractionshooter", "primary_weapons"))
+    val PISTOLS: TagKey<Item> = TagKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath("extractionshooter", "pistols"))
+    val GUNS: TagKey<Item> = TagKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath("extractionshooter", "guns"))
 }
